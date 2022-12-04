1ba9e3c9b1e9756b63d31b71efae80e2a8c993a1
licetullmann <licetrosales@gmail.com>
2022-12-04 20:38:37 +0100

    Initial commit

---
+++ src/main/java/com/gitbub/licetrosales/hellospringcontrollerlu/HelloSpringControllerLuApplication.java
@@ -0,0 +1,13 @@
+package com.gitbub.licetrosales.hellospringcontrollerlu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringControllerLuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringControllerLuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitbub/licetrosales/hellospringcontrollerlu/HelloSpringControllerLuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitbub.licetrosales.hellospringcontrollerlu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringControllerLuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
